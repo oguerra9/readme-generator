55d3c0fe2822dab9a50581fea2b484c16e723ff9
oliviaguerra731@gmail.com <102640672+nightbird99@users.noreply.github.com>
2022-07-23 13:16:42 -0400

    completed writing inquirer prompts and started markdown formatting

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,83 @@
 // TODO: Include packages needed for this application
-const inquirer = require(inquirer);
+const inquirer = require('inquirer');
+const fs = require('fs');
+const genMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Please enter a description of your project: ',
+            name: 'descript',
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions for using your project: ',
+            name: 'usageIns',
+        },
+        {
+            type: 'input',
+            message: 'Did anyone else contribute to this project? If so',
+            name: 'whatLearn',
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            name: 'installSteps',
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions and examples for use.', 
+            name: 'usageInstruct',
+        },
+        // {
+        //     type: 'input',
+        //     message: 'Enter the relative pathway to any screenshot images you would like to include.',
+        //     name: 'imgPath',
+        // },
+        {
+            type: 'input',
+            message: 'Enter any guidelines necessary for any developers who may want to contribute to your project: ',
+            name: 'contributing',
+        },
+        {
+            type: 'list',
+            message: 'What kind of license would you like to use for your project?',
+            choices: ['GNU AGPLv3','GNU GPLv3','Mozilla Public License 2.0','Apache License 2.0','MIT License'],
+            name: 'licenseChoice',
+        },
+        {
+            type: 'input',
+            message: 'Provide any examples on how to run any tests you may have included for your project here:', 
+            name: 'testEx',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'gitUser',
+        }
+    ])
+    .then((data) =>
+        writeToFile('README.md',generateMarkdown.generateMarkdown(data))
+    );
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(filename, data, (err) =>
+    err ? console.log(err) : console.log('README file created successfully'))
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  console.log(license);
+  var badge = '';
+  if (license == null) {
+    badge = '';
+    return badge;
+  }
+  else {
+
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  var licenseLink = '';
+  if (license == null) {
+    licenseLink = '';
+    return licenseLink;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  var licenseSection = '';
+  if (license == null) {
+    licenseSection = '';
+    return licenseSection;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}\n\n ## Description \n\n ${data.descript}\n\n ## Table of Contents\n\n[Installation](#installation)\n[Usage](#usage)\n[Contributing](#contributing)\n[Tests](#tests)\n[Questions](#questions)
+  \n\n## Installation
 
-`;
+  \n\n## Usage
+
+  \n\n## Contributing
+
+  \n\n## Tests
+
+  \n\n## Questions
+
+  
+  `;
 }
 
 module.exports = generateMarkdown;
